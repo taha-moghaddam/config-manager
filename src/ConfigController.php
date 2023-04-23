cadd6df90d1dce8220dea5a71db03c0d7638458e
mtmoghaddam <m5taha5m@gmail.com>
2023-04-23 14:36:13 +0330

    feat: Add more filters and translations

--- src/ConfigController.php
+++ src/ConfigController.php
@@ -21,8 +21,8 @@ class ConfigController
     public function index(Content $content)
     {
         return $content
-            ->header('Config')
-            ->description('list')
+            ->header(__('admin.config'))
+            ->description(__('admin.list'))
             ->body($this->grid());
     }
 
@@ -37,8 +37,8 @@ class ConfigController
     public function edit($id, Content $content)
     {
         return $content
-            ->header('Config')
-            ->description('edit')
+            ->header(__('admin.config'))
+            ->description(__('admin.edit'))
             ->body($this->form()->edit($id));
     }
 
@@ -52,23 +52,23 @@ class ConfigController
     public function create(Content $content)
     {
         return $content
-            ->header('Config')
-            ->description('create')
+            ->header(__('admin.config'))
+            ->description(__('admin.create'))
             ->body($this->form());
     }
 
     public function show($id, Content $content)
     {
         return $content
-            ->header('Config')
-            ->description('detail')
+            ->header(__('admin.config'))
+            ->description(__('admin.detail'))
             ->body(Admin::show(ConfigModel::findOrFail($id), function (Show $show) {
-                $show->id();
-                $show->name();
-                $show->value();
-                $show->description();
-                $show->created_at();
-                $show->updated_at();
+                $show->id(__('admin.id'));
+                $show->name(__('admin.name'));
+                $show->value(__('admin.value'));
+                $show->description(__('admin.description'));
+                $show->created_at(__('admin.created_at'));
+                $show->updated_at(__('admin.updated_at'));
             }));
     }
 
@@ -76,20 +76,24 @@ class ConfigController
     {
         $grid = new Grid(new ConfigModel());
 
-        $grid->id('ID')->sortable();
-        $grid->name()->display(function ($name) {
+        $grid->id(__('admin.id'))->sortable();
+        $grid->name(__('admin.name'))->display(function ($name) {
             return "<a tabindex=\"0\" class=\"btn btn-xs btn-twitter\" role=\"button\" data-toggle=\"popover\" data-html=true title=\"Usage\" data-content=\"<code>config('$name');</code>\">$name</a>";
-        });
-        $grid->value();
-        $grid->description();
+        })->sortable();
+        $grid->value(__('admin.value'))->sortable();
+        $grid->description(__('admin.description'));
 
-        $grid->created_at();
-        $grid->updated_at();
+        $grid->updated_at(__('admin.updated_at'))->sortable();
 
         $grid->filter(function ($filter) {
-            $filter->disableIdFilter();
-            $filter->like('name');
-            $filter->like('value');
+            $filter->column(1 / 2, function ($filter) {
+                $filter->like('name', __('admin.name'));
+            });
+
+            $filter->column(1 / 2, function ($filter) {
+                $filter->like('value', __('admin.value'));
+                $filter->like('description', __('admin.description'));
+            });
         });
 
         return $grid;
@@ -99,17 +103,17 @@ class ConfigController
     {
         $form = new Form(new ConfigModel());
 
-        $form->display('id', 'ID');
-        $form->text('name')->rules('required');
+        $form->display('id', __('admin.id'));
+        $form->text('name', __('admin.name'))->rules('required');
         if (config('admin.extensions.config.valueEmptyStringAllowed', false)) {
-            $form->textarea('value');
+            $form->textarea('value', __('admin.value'));
         } else {
-            $form->textarea('value')->rules('required');
+            $form->textarea('value', __('admin.value'))->rules('required');
         }
-        $form->textarea('description');
+        $form->textarea('description', __('admin.description'));
 
-        $form->display('created_at');
-        $form->display('updated_at');
+        $form->display('created_at', __('admin.created_at'));
+        $form->display('updated_at', __('admin.updated_at'));
 
         return $form;
     }
